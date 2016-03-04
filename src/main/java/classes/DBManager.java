6131816a7008858ae29b2519ba49a24732579ee7
logzee <misham.exe@gmail.com>
2016-03-04 22:30:31 +0300

    Created index.jsp & addevent.jsp

--- src/main/java/classes/DBManager.java
+++ src/main/java/classes/DBManager.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
  * Created by logzee on 04.03.16.
  */
 public class DBManager {
+    private final String URI = "127.10.188.130";
     private final String USERNAME = "admin";
     private final String DATABASE = "mineme";
     private final char[] PASSWORD = "9s__XpG8Yrvp".toCharArray();
@@ -19,7 +20,7 @@ public class DBManager {
 
     public DBManager() throws UnknownHostException {
         credential = MongoCredential.createCredential(USERNAME, DATABASE, PASSWORD);
-        mongoClient = new MongoClient(new ServerAddress(), Arrays.asList(credential));
+        new MongoClient(new MongoClientURI(System.getenv("OPENSHIFT_MONGODB_DB_URL")));
         db = mongoClient.getDB( "mineme" );
         coll = db.getCollection("testCollection");
     }
