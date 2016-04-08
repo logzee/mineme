8110edfce444fa72c3722479b6819fbc2c19d293
logzee <misham.exe@gmail.com>
2016-04-08 23:52:22 +0300

    Removed big chart margin

--- src/main/webapp/js/charts.js
+++ src/main/webapp/js/charts.js
@@ -104,7 +104,7 @@ function drawStepsChart() {
             title: 'Количество шагов',
             scaleType: 'mirrorLog'
         },
-        height: 350,
+        height: 270,
         legend: { position: 'none' },
         chartArea: {
             top: 55,
@@ -148,7 +148,7 @@ function drawSleepChart() {
             title: 'Качество сна',
             scaleType: 'mirrorLog'
         },
-        height: 350,
+        height: 270,
         legend: { position: 'none' },
         chartArea: {
             top: 55,
